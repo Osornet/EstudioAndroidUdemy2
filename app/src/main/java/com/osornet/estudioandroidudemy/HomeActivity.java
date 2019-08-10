4858766ef17a5db4ba593f64234992e71d96c2eb
osornet <osornet@gmail.com>
2019-08-10 13:01:55 -0500

    23. ListView & eventos de lista

--- app/src/main/java/com/osornet/estudioandroidudemy/HomeActivity.java
+++ app/src/main/java/com/osornet/estudioandroidudemy/HomeActivity.java
@@ -3,44 +3,87 @@ package com.osornet.estudioandroidudemy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    //views
-    private TextView tvEmail;
+public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    private ListView lvLista;
+    private List<String> androidVersionList;
+
+    private String email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mapViews();
-        showEmail(); // mostramos el email
+        setEmail();
+        showList();
+
     }
 
     /**
      * mapeo de views de la activity
      */
     private  void mapViews(){
-        this.tvEmail = findViewById(R.id.tvEmail);
+        lvLista = findViewById(R.id.lvLista);
     }
 
     /**
-     * muestra el email que se obtiene en el login
+     * metodo que setea el email cargandolo de los sharedpreferences
      */
-    private void showEmail(){
-        String email;
-        Bundle extras;
+    private void setEmail(){
 
-        //obtenermos el conjunto de extras que se envian en le intent del login
-        extras = getIntent().getExtras();
-        //obtenemos el email
-        email = extras.getString(MainActivity.EMAIL_LOGIN_KEY, "");
-        //si no esta vacio el email editamo el evEmail.
-        if(!email.isEmpty()){
-            email = "Bienvenido: ".concat(email);
-            this.tvEmail.setText(email);
+        Bundle extras = getIntent().getExtras();
+        this.email = extras.getString(MainActivity.EMAIL_LOGIN_KEY);
+    }
+
+    /**
+     * muestra la lista de elementos
+     */
+    private void showList(){
+        this.androidVersionList = new ArrayList<>();
+
+        //cambialos el titulo del toolbar con el email del usuario
+        if(this.email != null && !this.email.isEmpty()){
+            setTitle(this.email);
         }
 
+        //carga de la lista de elementos
+        androidVersionList.add("pie");
+        androidVersionList.add("Oreo");
+        androidVersionList.add("Nougat");
+        androidVersionList.add("Marshmallow");
+        androidVersionList.add("Lollipop");
+        androidVersionList.add("Kitkat");
+        androidVersionList.add("...");
+
+        //adaptador
+        ArrayAdapter<String> adaptadorVersionesAndroid = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                androidVersionList
+        );
+        //vinculacion del adaptador a su respectiva view
+        this.lvLista.setAdapter(adaptadorVersionesAndroid);
+
+        //eventos click de la lista
+        this.lvLista.setOnItemClickListener(this);
+}
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String androidVersion = this.androidVersionList.get(position);
+        Log.i("APP", "Version click: ".concat(androidVersion));
     }
 }
